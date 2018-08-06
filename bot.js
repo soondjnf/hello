f808ef07f3bda32214a93f540728fe867306bc9c
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-08-06 15:55:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,16 +17,16 @@ client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
     
-if (message.content.startsWith(adminprefix + 'vip setgame')) {
+if (message.content.startsWith(adminprefix + 'vipsetgame')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
 } else 
-  if (message.content.startsWith(adminprefix + 'vip name')) {
+  if (message.content.startsWith(adminprefix + 'vipname')) {
 client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-  if (message.content.startsWith(adminprefix + 'vip avatar')) {
+  if (message.content.startsWith(adminprefix + 'vipavatar')) {
 client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
       } else     
